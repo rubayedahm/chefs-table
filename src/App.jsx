0e2697097eb16e669ffcea7acd3d1b2245ae3011
Rubayed Ahmed <rubayed.ahmed02@gmail.com>
2024-09-15 17:45:23 +0600

    navbar complete

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,16 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navbar from './components/Navbar/Navbar'
+import Banner from './components/Banner/Banner'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-   
-      <h1 className='text-3xl'>Vite + React</h1>
+      <Navbar/>
+      <Banner/>
     
     </>
   )
---
+++ src/components/Navbar/Navbar.jsx
@@ -0,0 +1,59 @@
+import React from 'react';
+import { CgProfile } from "react-icons/cg";
+
+const Navbar = () => {
+    return (
+        <div className="navbar bg-base-100">
+  <div className="navbar-start">
+    <div className="dropdown">
+      <div tabIndex={0} role="button" className="btn btn-ghost lg:hidden">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          className="h-5 w-5"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor">
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h8m-8 6h16" />
+        </svg>
+      </div>
+      <ul
+        tabIndex={0}
+        className="menu menu-sm dropdown-content bg-base-100 rounded-box z-[1] mt-3 w-52 p-2 shadow">
+        <li><a>Item 1</a></li>
+        <li>
+          <a>Parent</a>
+          <ul className="p-2">
+            <li><a>Submenu 1</a></li>
+            <li><a>Submenu 2</a></li>
+          </ul>
+        </li>
+        <li><a>Item 3</a></li>
+      </ul>
+    </div>
+    <a className="btn btn-ghost text-xl">Recipe Calories</a>
+  </div>
+  <div className="navbar-center hidden lg:flex">
+    <ul className="menu menu-horizontal px-1">
+      <li><a className='text-[#150B2BB0]'>Home</a></li>
+      <li><a className='text-[#150B2BB0]'>Recipes</a></li>
+      <li><a className='text-[#150B2BB0]'>About</a></li>
+      <li><a className='text-[#150B2BB0]'>Search</a></li>
+    </ul>
+  </div>
+  <div className="navbar-end">
+  <div className="form-control">
+      <input type="text" placeholder="Search" className="input input-bordered w-24 md:w-auto p-4 h-10 mr-3 rounded-full bg-[#150B2B0D]" />
+    </div>
+    <div className='text-2xl bg-[#0BE58A] rounded-full p-2'>
+    <CgProfile />
+    </div>
+  </div>
+</div>
+    );
+};
+
+export default Navbar;
